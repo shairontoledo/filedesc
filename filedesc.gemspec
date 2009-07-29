956328ef3acabb4e3df51a91c189ea90e678974b
shairon toledo <shairon.toledo@gmail.com>
2009-07-29 18:06:27 -0400

    Initial files but already are working

---
+++ filedesc.gemspec
@@ -0,0 +1,17 @@
+require 'rubygems'
+
+SPEC = Gem::Specification.new do |s|
+   s.name = "filedesc"
+   s.version = "0.0.2"
+   s.author = "Shairon Toledo"
+   s.email = "shairon.toledo@gmail.com"
+   s.homepage = "http://github.com/shairontoledo/filedesc"
+   s.platform = Gem::Platform::RUBY
+   s.summary = "A wrapper that works with ruby File class and unix command 'file' to get dynamic mime-type and content description"
+   candidates = Dir.glob("lib/**/*")
+   s.files = candidates.delete_if do |item|
+      item.include?("CVS") || item.include?("rdoc")
+   end
+   s.require_path  = "lib"
+   s.has_rdoc = false
+end
---
+++ lib/filedesc.rb
@@ -0,0 +1,63 @@
+module FileDescription
+
+  OPTIONS = {:executable => '/usr/bin/file' }
+
+  def mime
+    @mime ||= get_output('-I')
+    @mime
+  end
+  def is_a_mime?(type)
+    return case type
+    when String
+      mime == type
+    when Regexp
+      !(mime !~ type)
+    end
+  end
+
+  def description
+    @description ||= get_output
+    @description
+  end
+
+  def has_description?(regexp)
+    !(description !~ regexp)
+  end
+
+  
+  def extension
+    self.path.scan(/\.([A-za-z0-9]+)$/).to_s
+  end
+
+  def inspect
+    "#<File:#{self.path}, mime: '#{mime}', description: '#{description}', extension: '#{extension}' >"
+  end
+
+  def info
+      {
+        :filename =>   File.basename(self.path),
+        :mime =>   mime,
+        :description =>   description,
+        :extension =>   extension,
+        :directory =>  File.dirname(self.path),
+        :path =>  self.path,
+        :size => File.size(self.path),
+        :last_modified => File.mtime(self.path),
+        :last_access => File.atime(self.path),
+        :executable => File.executable?(self.path),
+        :gid => File.stat(self.path).gid,
+        :uid => File.stat(self.path).uid,
+        :mode => File.stat(self.path).mode
+
+      }
+  end
+  
+  private
+  def get_output(params="")
+    `#{OPTIONS[:executable]} -b #{params} '#{self.path}'`.strip
+  end
+end
+  
+class File
+  include FileDescription
+end
